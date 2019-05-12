fb178f1db28bb6a4f7f4d77c9cbf39e29eaabbef
phozzy <arthur.fayzullin@gmail.com>
2019-05-12 13:21:20 +0200

    build image with extensions

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 NEWCONTAINER=$(buildah --storage-driver vfs from postgres:${PG_VER})
 SCRATCHMNT=$(buildah --storage-driver vfs mount ${NEWCONTAINER})
-echo ${PWD}
-echo ${CI_PROJECT_DIR}
+podman --cgroup-manager=cgroupfs --storage-driver vfs run --systemd=false -v ${SCRATCHMNT}:/mnt:rw -v ${CI_PROJECT_DIR}:/root/project:ro --env DESTDIR=/mnt registry.gitlab.com/thehyve/ci_images/of_pg_dev:${PG_VER} bash -c "cd /root/project && make install"
+buildah --storage-driver vfs unmount ${NEWCONTAINER}
+buildah --storage-driver vfs commit ${NEWCONTAINER} ${CI_PROJECT_NAME}
+buildah --storage-driver vfs tag ${CI_PROJECT_NAME} ${CI_REGISTRY_IMAGE}:${PG_VER}
+buildah --storage-driver vfs push --creds ${CI_REGISTRY_USER}:${CI_JOB_TOKEN} ${CI_REGISTRY_IMAGE}:${PG_VER}
