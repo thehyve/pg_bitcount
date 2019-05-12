684499a2db9c8d7a67ea120a2a426e5b3e58bd57
phozzy <arthur.fayzullin@gmail.com>
2019-05-12 13:32:44 +0200

    use rw mount for artifact directory

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 NEWCONTAINER=$(buildah --storage-driver vfs from postgres:${PG_VER})
 SCRATCHMNT=$(buildah --storage-driver vfs mount ${NEWCONTAINER})
-podman --cgroup-manager=cgroupfs --storage-driver vfs run --systemd=false -v ${SCRATCHMNT}:/mnt:rw -v ${CI_PROJECT_DIR}:/root/project:ro --env DESTDIR=/mnt registry.gitlab.com/thehyve/ci_images/of_pg_dev:${PG_VER} bash -c "cd /root/project && make install"
+podman --cgroup-manager=cgroupfs --storage-driver vfs run --systemd=false -v ${SCRATCHMNT}:/mnt:rw -v ${CI_PROJECT_DIR}:/root/project:rw --env DESTDIR=/mnt registry.gitlab.com/thehyve/ci_images/of_pg_dev:${PG_VER} bash -c "cd /root/project && make install"
 buildah --storage-driver vfs unmount ${NEWCONTAINER}
 buildah --storage-driver vfs commit ${NEWCONTAINER} ${CI_PROJECT_NAME}
 buildah --storage-driver vfs tag ${CI_PROJECT_NAME} ${CI_REGISTRY_IMAGE}:${PG_VER}
