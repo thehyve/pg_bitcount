018e17e2bf43186cedc9a0016f8de9d08b464e83
phozzy <arthur.fayzullin@gmail.com>
2019-09-13 11:25:58 +0000

    Define docker format for compatibility

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+export BUILDAH_FORMAT=docker
 NEWCONTAINER=$(buildah --storage-driver vfs from postgres:${PG_VER})
 SCRATCHMNT=$(buildah --storage-driver vfs mount ${NEWCONTAINER})
 podman --cgroup-manager=cgroupfs --storage-driver vfs run --systemd=false -v ${SCRATCHMNT}:/mnt:rw -v ${CI_PROJECT_DIR}:/root/project:rw --env DESTDIR=/mnt registry.gitlab.com/thehyve/ci_images/of_pg_dev:${PG_VER} bash -c "cd /root/project && make install"
